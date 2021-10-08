73fa7efd32ded63f4e45b53e09210c83a020fa84
Daniel <danielmtran.dev@gmail.com>
2021-10-08 20:19:19 +0100

    lostWithoutAMap

---
+++ lostWithoutAMap.js
@@ -0,0 +1,9 @@
+// Beginner - Lost Without a Map
+function maps(x){
+    let newX = []
+    for(let i = 0; i < x.length; i++){
+      let doubleX = x[i] * 2
+      newX.push(doubleX)
+    }
+    return newX
+  }
