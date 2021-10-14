38d281283444729638a10b2c64cef8d475c6fa58
Daniel <danielmtran.dev@gmail.com>
2021-10-13 23:39:42 -0400

    freudianTranslator

---
+++ freudianTranslator.js
@@ -0,0 +1,11 @@
+// Freudian Translator
+
+function toFreud(string) {
+    if(string === ''){
+      return ''
+    }
+    else{
+      let result = string.split(' ').map(x => x = 'sex').join(' ')
+      return result;
+    }
+}
