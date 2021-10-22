5b82447f873928985c7e9c87ba56a39f80a1b971
Daniel <danielmtran.dev@gmail.com>
2021-10-22 20:19:19 +0100

    trafficLight

---
+++ trafficLight.js
@@ -0,0 +1,17 @@
+// trafficLight
+
+function updateLight(current) {
+    if(current === 'green'){
+      current = 'yellow'
+      return current
+    }
+    else if(current === 'yellow'){
+      current = 'red'
+      return current
+    }
+    else if(current === 'red')
+      current = 'green'
+      return current
+    //your code here!
+  
+  }
