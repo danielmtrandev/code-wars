df3f62d85792a13c71dea6bfec9807972ae40490
Daniel <danielmtran.dev@gmail.com>
2021-10-23 20:19:19 +0100

    sumArrays

---
+++ sumArrays.js
@@ -0,0 +1,16 @@
+// sumArrays
+
+function sum (numbers) {
+    "use strict";
+  let sum = 0;
+  if(numbers === []){
+    return 0
+  }
+  else{
+  for(let i =0; i < numbers.length; i++){
+    sum = sum + numbers[i]
+    }
+  }
+  return sum
+    
+};
