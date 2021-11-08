9c9e80dbd23d8fbb8341b38366a32f2dd7f0ae97
Daniel <danielmtran.dev@gmail.com>
2021-11-07 23:42:05 -0500

    sumMixedArray

---
+++ sumMixedArray.js
@@ -0,0 +1,13 @@
+// Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
+// Return your answer as a number.
+
+function sumMix(x){
+    let arrNum = x.map(Number)
+    let sum = 0
+    for(let i = 0; i < arrNum.length; i++){
+      sum = sum + arrNum[i]
+    }
+    return sum
+    
+  
+}
