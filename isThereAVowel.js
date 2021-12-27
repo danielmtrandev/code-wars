09a3b7f8f5a6bf4e7df4db4deff833f0409b9bac
Daniel <danielmtran.dev@gmail.com>
2021-12-27 20:19:19 +0100

    code-wars

---
+++ isThereAVowel.js
@@ -0,0 +1,28 @@
+// Given an array of numbers, check if any of the numbers are the character codes for lower case vowels (a, e, i, o, u).
+
+// If they are, change the array value to a string of that vowel.
+
+// Return the resulting array.
+
+function isVow(a){
+    let vowels = 'aeiou'
+    for(let i = 0; i < a.length; i++) {
+      if(a[i] === vowels.charCodeAt(0)){
+        a[i] = 'a'
+      }
+      else if(a[i] === vowels.charCodeAt(1)){
+        a[i] = 'e'
+      }
+      else if(a[i] === vowels.charCodeAt(2)){
+        a[i] = 'i'
+      }
+      else if(a[i] === vowels.charCodeAt(3)){
+        a[i] = 'o'
+      }
+      else if(a[i] === vowels.charCodeAt(4)){
+        a[i] = 'u'
+      }
+    }
+    return a
+  
+  }
