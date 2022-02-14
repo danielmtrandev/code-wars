2d4478e32e493d06a0dbce038bebbb4284548d7c
Daniel <danielmtran.dev@gmail.com>
2022-02-14 20:19:19 +0100

    code-wars

---
+++ codingMeetup4.js
@@ -0,0 +1,7 @@
+
+
+
+function getFirstPython(list) {
+    let dev = list.find(x => x.language == 'Python')
+    return dev ? dev.firstName + ', ' + dev.country : 'There will be no Python developers'
+  }
