3d78022b6f97b9b8442ef55ec99c8f4345231eeb
Daniel <danielmtran.dev@gmail.com>
2022-03-05 20:19:19 +0100

    code-wars

--- absentVowel.js
+++ absentVowel.js
@@ -12,6 +12,24 @@
 // "John Doe hs seven red pples under his bsket"          =>  0  ; missing: "a"
 // "Bb Smith sent us six neatly arranged range bicycles"  =>  3  ; missing: "o"
 
+
+
+// P: string of words with a vowel missing
+// R: index of vowel missing from string
+// E: ("John Doe hs seven red pples under his bsket"), 0);
+// P: 
+
+
+function absentVowel(x){
+  let vowels = ['a', 'e', 'i', 'o', 'u']
+  
+//   return vowels.filter((v,i,a) => !x.includes(v) ? i : false)
+  let missing = vowels.filter((v,i,a) => !x.includes(v))
+  return vowels.indexOf(missing[0])
+}
+
+// OR
+
 function absentVowel(x){
     let vowels = ['a','e','i','o','u']
     let sentence = x.split('')
--- reversedStrings.js
+++ reversedStrings.js
@@ -6,6 +6,18 @@
 
 // "The greatest victory is that which requires no battle" --> "battle no requires which that is victory greatest The"
 
+
+function solution(str){
+  let newStr = str.split('')
+  let reverse = []
+  for(let i = newStr.length - 1; i >= 0; i--){
+    reverse.push(newStr[i])
+  }
+  return reverse.join('')
+}
+
+// OR
+
 function solution(str){
   return str.split('').reverse().join('')
 }
